72250696d2f84b845be821c9d642f8c56354fae8
Vishak Bhaskar <vishakbhaskar.619@gmail.com>
2023-04-01 23:11:07 +0530

    variables

---
+++ variables/target/debug/deps/variables-0f34dc8ae57f2e74.d
@@ -0,0 +1,5 @@
+/home/vishak/Projects/Real-Projects/rust-book/variables/target/debug/deps/variables-0f34dc8ae57f2e74: src/main.rs
+
+/home/vishak/Projects/Real-Projects/rust-book/variables/target/debug/deps/variables-0f34dc8ae57f2e74.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/vishak/Projects/Real-Projects/rust-book/variables/target/debug/variables: /home/vishak/Projects/Real-Projects/rust-book/variables/src/main.rs
